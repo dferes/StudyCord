e44fbc48d99a3e8532590a80dfe3c03964bc35a0
dferes <dferes23@gmail.com>
2021-11-05 11:08:52 -0700

    added a view for edit-user

--- StudyCord/base/views.py
+++ StudyCord/base/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def login_page(request):
@@ -185,3 +185,14 @@ def user_profile(request, pk):
     }
     return render(request, 'base/profile.html', context)
  
+
+@login_required(login_url='/login')
+def update_user(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form': form})
