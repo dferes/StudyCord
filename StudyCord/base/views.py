7b4e3c09b7e6604c7be49003893c31cca67103f7
dferes <dferes23@gmail.com>
2021-11-12 22:12:08 -0800

    added endpoints for topcs_page and acrivity_page

--- StudyCord/base/views.py
+++ StudyCord/base/views.py
@@ -65,7 +65,7 @@ def home(request):
         Q(description__icontains=q)
     ) 
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5] # Just limit to the first 5 for now. Maybe organize by most popular later
     room_count = rooms.count()  # apparently, getting the length of a query set is faster than python's len()
     # room_messages = Message.objects.all()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -196,3 +196,14 @@ def update_user(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update_user.html', {'form': form})
+
+
+def topics_page(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activity_page(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
