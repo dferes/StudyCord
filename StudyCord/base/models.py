c0f247be42bdbf56d6fca34453856ff6f3977bc9
dferes <dferes23@gmail.com>
2021-11-18 21:28:47 -0800

    made a custom user class model that inherits from the built in user model.

--- StudyCord/base/models.py
+++ StudyCord/base/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User # Django already has the model included
+from django.contrib.auth.models import AbstractUser # Django already has the model included
 from django.db.models.deletion import CASCADE # Do I need this import? (auto generated when 'cascade' was used) 
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default='avatar.svg') # relies on a third party package called 'pillow', which is an image processing library
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
