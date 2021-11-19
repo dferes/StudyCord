70125cd2e251160d02792c587692c1f65b277b43
dferes <dferes23@gmail.com>
2021-11-18 21:25:30 -0800

    modified root urls to accomodate uploaded user media stored in static folder.

--- StudyCord/StudyCord/urls.py
+++ StudyCord/StudyCord/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 """StudyCord URL Configuration
 
@@ -21,4 +23,7 @@ Including another URLconf
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
